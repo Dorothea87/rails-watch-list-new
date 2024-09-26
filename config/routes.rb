b136049439548fc29582fd1e8f996601b7535285
Dorothea Toeller <toellerdorothea@gmail.com>
2024-09-26 18:10:09 +0100

    add new list functionality

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   resources :movies
+  resources :lists
+  resources :lists do resources :bookmarks, only: [:new, :create]
+  end
 end
